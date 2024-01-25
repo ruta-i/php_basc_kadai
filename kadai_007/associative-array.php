a2e7f3d1ca398417cf83ea7ce8ced9bd8f6765d2
ruta-i <130815861+ruta-i@users.noreply.github.com>
2024-01-26 06:06:57 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='ja'>
+    <head>
+        <meta charset='utf-8'>
+        <title>kadai_007</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            //連想配列を定義し、キーと値を代入する
+            $item =['name' =>'onion','price' =>200,'weight' =>160];
+
+            //連想配列を出力する
+            print_r($item);
+            ?>
+        </p>
+    </body>
+</html>
